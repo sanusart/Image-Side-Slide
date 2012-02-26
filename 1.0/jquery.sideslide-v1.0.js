02625172bff2605b08bc2547ef85a4d5b56d662c
operad <alex@operad.com>
2012-02-26 08:50:21 +0200

    Clean-up of unnided comments. Add minified version.

--- 1.0/jquery.sideslide-v1.0.js
+++ 1.0/jquery.sideslide-v1.0.js
@@ -1,75 +1,72 @@
 /**
- * @Name:       Image Side Slide
- * @Version:    1.0
- * @Dependency: jQuery 1.6+
- * @Author:     Sasha Khamkov
- * @URL:        http://sanusart.com
- * @License:    The MIT License (MIT) http://www.opensource.org/licenses/MIT 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Usage:  a) include jQuery in your document.
-        b) include this file.
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Example: 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-jQuery:
-        $(document).ready(function () {
-            $('.mydiv').sideslide({
-                image: 'http://placehold.it/600x150&text=This+is+test+of+%22Image+Side+Slide%22+Plug-in',
-                duration: 5000,
-                startPoint: '0px',
-                toPoint: '680px',
-                backPoint: '0px'
-            });
-        });
---------------------------------------------------
-HTML:
-        <div class="mydiv"></div>
---------------------------------------------------
-CSS:
-        .mydiv {
-            border: none;
-            cursor: pointer;
-            height: 150px;
-            width: 150px;
-        }
---------------------------------------------------        
-*/
+ * Name:       Image Side Slide
+ * Version:    1.0
+ * Dependency: jQuery 1.6+
+ * Author:     Sasha Khamkov
+ * URL:        http://sanusart.com
+ * License:    The MIT License (MIT) - http://www.opensource.org/licenses/MIT
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ Usage:  a) include jQuery in your document.
+ b) include this file.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ Example:
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ jQuery:
+ $(document).ready(function () {
+ $('.mydiv').sideslide({
+ image: 'http://placehold.it/600x150&text=This+is+test+of+%22Image+Side+Slide%22+Plug-in',
+ duration: 5000,
+ startPoint: '0px',
+ toPoint: '680px',
+ backPoint: '0px'
+ });
+ });
+ --------------------------------------------------
+ HTML:
+ <div class="mydiv"></div>
+ --------------------------------------------------
+ CSS:
+ .mydiv {
+ border: none;
+ cursor: pointer;
+ height: 150px;
+ width: 150px;
+ }
+ --------------------------------------------------
+ */
 (function($) {
-  $.fn.extend({
-    
-    sideslide: function(options) {
+	$.fn.extend({
 
-      var options = $.extend(defaults, options);
+		sideslide : function(options) {
 
-      return this.each(function() {
-        var o = options;
-        var _elem_ = $(this);
-        if (o.initialPosition !== '') {
-          _elem_.css('backgroundPosition', o.startPoint);
-        }
-        _elem_.css('background-image', 'url(' + o.image + ')');
-        _elem_.click(function() {
-          _elem_.animate({
-            'backgroundPosition': o.toPoint
-          }, {
-            duration: o.duration
-          }), _elem_.animate({
-            'backgroundPosition': o.backPoint
-          }, {
-            duration: o.duration
-          });
-        });
+			var options = $.extend(defaults, options);
 
-      });
+			return this.each(function() {
+				var o = options;
+				var _elem_ = $(this);
+				if(o.initialPosition !== '') {
+					_elem_.css('backgroundPosition', o.startPoint);
+				}
+				_elem_.css('background-image', 'url(' + o.image + ')');
+				_elem_.click(function() {_elem_.animate({
+						'backgroundPosition' : o.toPoint
+					}, {
+						duration : o.duration
+					}), _elem_.animate({
+						'backgroundPosition' : o.backPoint
+					}, {
+						duration : o.duration
+					});
+				});
+			});
+			var defaults = {
+				image : 'http://placehold.it/600x150&text=This+is+test+of+%22Image+Side+Slide%22+Plug-in',
+				duration : 5000,
+				startPoint : '0px',
+				toPoint : '680px',
+				backPoint : '0px'
+			}
 
-      var defaults = {
-        image: 'http://placehold.it/600x150&text=This+is+test+of+%22Image+Side+Slide%22+Plug-in',
-        duration: 5000,
-        startPoint: '0px',
-        toPoint: '680px',
-        backPoint: '0px'
-      }
-
-    }
-  });
+		}
+	});
 })(jQuery);
